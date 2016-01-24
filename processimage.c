6f19a2cb0a061ecdf246c01a9f66983476af28f8
Simon Vergara <simon.vergara.1993@gmail.com>
2016-01-24 11:44:55 -0500

    successfully changed it so that the average filter works almost perfectly. The size of the panel can be any odd number (will be an input in the future). Only need to fix up the pixels between processes

--- processimage.c
+++ processimage.c
@@ -16,7 +16,7 @@ void processImage(int width, int height, RGB *image, int my_rank)
     int my_end = width*height;
 
     int myrow;
-    int n = 3;
+    int n = 5;
 
     RGB *out = (RGB*)malloc(my_end*sizeof(RGB));
 
@@ -29,27 +29,34 @@ void processImage(int width, int height, RGB *image, int my_rank)
 
         RGB *po = image + i;
 
+
         for (int y = (n/2); y > ((n/2)-n); y--)
-        {//loops columns
+        {//loops within columns
+            myrow = (i-y*width)/width;
             for (int x = (n/2); x > ((n/2)-n) ; x--)
-            {//loops rows
+            {//loop within rows
+                if (((i-y*width - x) <= 0) || ((i-y*width - x) >= my_end)) continue;
+
+                int testrow = (i-y*width - x)/width;
+
+                if (myrow != testrow) continue;
+
+                panecount ++;
                 RGB *pi = po - y*width - x;
-//printf("R%d G%d B%d\n", pi->r, pi->g, pi->b);
+
                 sumr += pi->r;
                 sumb += pi->b;
                 sumg += pi->g;
             }//rows
         }//columns
 
-//printf("sumr%d sumg%d sumb%d\n", sumr,sumg,sumb);
 
+//printf("i%d panecount%d\n", i,panecount);
 
         RGB *final = out+i;
-        final->r = sumr/(n*n);
-        final->g = sumg/(n*n);
-        final->b = sumb/(n*n);
-
-
+        final->r = sumr/(panecount);
+        final->g = sumg/(panecount);
+        final->b = sumb/(panecount);
 
     }//pixels
 
@@ -63,66 +70,9 @@ void processImage(int width, int height, RGB *image, int my_rank)
         last->b = temp->b;
      }
 
+
+
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-void dumbfunct(int width, int height, RGB *image, int my_rank)
-{
-// simple image "processing"
-    int i;
-    int my_start = 0;
-    int my_end = width*height;
-int modran=0;
-//printf("P%d: w%d h%d started processing with my_start%d my_end%d\n", 
-//    my_rank, width, height, my_start, my_end);
-
-modran = my_rank % 3;
-    if(modran==0){
-        for (i=my_start; i < my_end; i++)
-        {
-            RGB *p = image+i;
-            //printf("BEF: Pixel %d: R%d, G%d, B%d.\n", i,p->r, p->g, p->b);
-            p->g ++;
-            p->b ++;
-            p->r = 255;
-            //printf("AFT: Pixel %d: R%d, G%d, B%d.\n", i,p->r, p->g, p->b);
-        }
-    }
-    else if(modran==1){
-        for (i=my_start; i < my_end; i++)
-        {
-            RGB *p = image+i;
-            //printf("P1 BEF: Pixel %d: R%d, G%d, B%d.\n", i,p->r, p->g, p->b);
-            p->b = 255;
-            p->g ++;
-            p->r ++;
-            //printf("P1 AFT: Pixel %d: R%d, G%d, B%d.\n", i,p->r, p->g, p->b);
-        }
-    }
-    else if(modran==2){
-        for (i=my_start; i < my_end; i++)
-        {
-            RGB *p = image+i;
-            //printf("P1 BEF: Pixel %d: R%d, G%d, B%d.\n", i,p->r, p->g, p->b);
-            p->g = 255;
-            p->b ++;
-            p->r ++;
-            //printf("P1 AFT: Pixel %d: R%d, G%d, B%d.\n", i,p->r, p->g, p->b);
-        }
-    }
-
-//    printf("P%d: done processing\n", my_rank);
-}
